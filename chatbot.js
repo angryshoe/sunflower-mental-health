b46624e2f8970a60ad0891323d4201e71a808c5d
angryshoe <52721767+angryshoe@users.noreply.github.com>
2019-07-23 14:09:07 -0700

    Add files via upload

--- chatbot.js
+++ chatbot.js
@@ -4,32 +4,10 @@ var question = '<h1>Hello! I am Support Bot, KAYA. I have a number of functions.
 var output = document.getElementById('output');				// store id="output" in output variable
 output.innerHTML = question;													// ouput first question
 
-function bot() {
-    var input = document.getElementById("input").value;
-    console.log(input);
-
-    if (questionNum == 0) {
-    output.innerHTML = '<h1>hello ' + input + '</h1>';// output response
-    document.getElementById("input").value = "";   		// clear text box
-    question = '<h1>how old are you?</h1>';			    	// load next question
-    setTimeout(timedQuestion, 2000);									// output next question after 2sec delay
-    }
-
-    else if (questionNum == 1) {
-    output.innerHTML = '<h1>That means you were born in ' + (2016 - input) + '</h1>';
-    document.getElementById("input").value = "";
-    question = '<h1>where are you from?</h1>';
-    setTimeout(timedQuestion, 2000);
-    }
-}
-
-function timedQuestion() {
-    output.innerHTML = question;
-}
-
 var optionOne;
 var optionTwo;
 var optionThree;
+
 function optionOne() {
   output.innerHTML = "<h1>Please get immediate help if you are in danger. Our resources tab has plenty of hotlines and pages for help if you need it. The national suicide crisis hotline for the U.S. is 1-800-273-8255.</h1>";v
 }
@@ -55,8 +33,32 @@ function branchTwo() {
   branch.remove();
   document.getElementById("input").type = "text";
   output.innerHTML = "<h1>Okay! I'm open to talk. To reintroduce myself, my name is KAYA and I am a support chatbot. What is your name?<h1>";
+};
+
+function bot() {
+    var input = document.getElementById("input").value;
+    console.log(input);
+
+    if (questionNum == 0) {
+    output.innerHTML = '<h1>Nice to meet you, ' + input + '!</h1>';// output response
+    document.getElementById("input").value = "";   		// clear text box
+    question = '<h1>how old are you?</h1>';			    	// load next question
+    setTimeout(timedQuestion, 2000);									// output next question after 2sec delay
+    }
+
+    else if (questionNum == 1) {
+    output.innerHTML = '<h1>That means you were born in ' + (2016 - input) + '</h1>';
+    document.getElementById("input").value = "";
+    question = '<h1>where are you from?</h1>';
+    setTimeout(timedQuestion, 2000);
+    }
 }
 
+function timedQuestion() {
+    output.innerHTML = question;
+}
+
+//push enter key (using jquery), to run bot function.
 $(document).keypress(function(e) {
   if (e.which == 13) {
     bot();																						// run bot function when enter key pressed
