4b6d693994edf0b17525a70fa570a8bab160ddd5
angryshoe <52721767+angryshoe@users.noreply.github.com>
2019-07-24 14:52:51 -0700

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -7,6 +7,14 @@ output.innerHTML = question;													// ouput first question
 var optionOne;
 var optionTwo;
 var optionThree;
+var itBeLikeThat;
+var response;
+var responsesOne = ["<h1>It's never a bad idea to focus on the positive<h1>", "<h1>I'm so glad you're here to talk to m<h1>", "<h1>yeet<h1>"];
+var responsesTwo = ["<h1>Your feelings are valid!<h1>", "<h1>That's pretty interesting!<h1>", "<h1>I'll always be here.<h1>"];
+var responsesThree = ["<h1>Take care of yourself<h1>", "<h1>Wow.<h1>", "<h1>How are you feeling?<h1>"];
+var lifeCheck;
+var lifeCheckTwo;
+var inputTwo;
 
 function optionOne() {
   output.innerHTML = "<h1>Please get immediate help if you are in danger. Our resources tab has plenty of hotlines and pages for help if you need it. The national suicide crisis hotline for the U.S. is 1-800-273-8255.</h1>";v
@@ -38,30 +46,108 @@ function branchTwo() {
 function bot() {
     var input = document.getElementById("input").value;
     console.log(input);
+    console.log(questionNum);
+    var inputTwo = input.toLowerCase();
+    var lifeCheck = inputTwo.search("kill");
+    var lifeCheckTwo = inputTwo.search("suicide");
 
     if (questionNum == 0) {
     output.innerHTML = '<h1>Nice to meet you, ' + input + '!</h1>';// output response
     document.getElementById("input").value = "";   		// clear text box
-    question = '<h1>how old are you?</h1>';			    	// load next question
-    setTimeout(timedQuestion, 2000);									// output next question after 2sec delay
+    question = '<h1>How was your day?</h1>';			    	// load next question
+    thinking();
+    setTimeout(timedQuestion, 2250);									// output next question after 2sec delay
+    }
+
+    if (lifeCheck != -1 || lifeCheckTwo != -1){
+      output.innerHTML = "<h1>Please think before doing anything rash. If you or someone else is in danger, I highly recommend seeking help.<h1>";
+      question = '<h1>Is everything okay?<h1>';
+      questionNum = 1;
+      thinking();
+      setTimeout(timedQuestion, 2250);
     }
 
     else if (questionNum == 1) {
-    output.innerHTML = '<h1>That means you were born in ' + (2016 - input) + '</h1>';
+    output.innerHTML = '<h1>Well, everyday is a new day! Always try to remember the good.</h1>';
     document.getElementById("input").value = "";
-    question = '<h1>where are you from?</h1>';
-    setTimeout(timedQuestion, 2000);
+    question = '<h1>Are you doing okay?</h1>';
+    thinking();
+    setTimeout(timedQuestion, 2250);
     }
-}
+
+    else if (questionNum == 2) {
+      var check = inputTwo.search("no");
+      var checkTwo = inputTwo.search("yes");
+        if (check != -1){
+          output.innerHTML = "<h1>Oh no.<h1>";
+          question = '<h1>Do you want to talk about it?<h1>';
+          thinking();
+          setTimeout(timedQuestion,2250);
+        }
+        if (checkTwo != -1){
+            output.innerHTML = "<h1>Glad to hear that!<h1>";
+            question = 'responsesOne';
+            thinking();
+            setTimeout(timedQuestion, 2250);
+          }
+        else if (check != -1 && checkTwo != -1){
+          output.innerHTML = "<h1>Hm?<h1>";
+          var randomAns = responsesTwo[Math.floor(Math.random()*responsesOne.length)];
+          question = randomAns;
+          thinking();
+          setTimeout(timedQuestion, 2250);
+          }
+      }
+      else if (questionNum == 3) {
+        console.log("Q3");
+        var okCheck = inputTwo.search("yes");
+        if (okCheck != -1) {
+          question = "<h1>I'm all ears!<h1>";
+          thinking();
+          setTimeout(timedQuestion, 2250)
+        }
+        else {
+          var randomAns = responsesTwo[Math.floor(Math.random()*responsesOne.length)];
+          question = randomAns;
+          thinking();
+          setTimeout(timedQuestion,2250);
+        }
+      }
+      else if ((questionNum >= 4) && (questionNum % 3 == 0)) {
+        var randomAns = responsesOne[Math.floor(Math.random()*responsesOne.length)];
+        question = randomAns;
+        thinking();
+        setTimeout(timedQuestion,2250);
+      }
+      else if ((questionNum >= 4) && (questionNum % 3 == 1)) {
+        var randomAns = responsesTwo[Math.floor(Math.random()*responsesTwo.length)];
+        question = randomAns;
+        thinking();
+        setTimeout(timedQuestion,2250);
+      }
+      else if ((questionNum >= 4) && (questionNum % 3 == 2)) {
+        var randomAns = responsesThree[Math.floor(Math.random()*responsesThree.length)];
+        question = randomAns;
+        thinking();
+        setTimeout(timedQuestion, 2250);
+      }
+     }
 
 function timedQuestion() {
     output.innerHTML = question;
+    document.getElementById("input").type = "text";
+    document.getElementById("thoughts").style.display = "none";
+}
+
+function thinking(){
+  document.getElementById("input").type = "hidden";
+  document.getElementById("thoughts").style.display = "block";
 }
 
 //push enter key (using jquery), to run bot function.
 $(document).keypress(function(e) {
   if (e.which == 13) {
     bot();																						// run bot function when enter key pressed
-    questionNum++;																		// increase questionNum count by 1
+    questionNum+=1;																		// increase questionNum count by 1
   }
 });
