e74ac34a4b65191ca39cdae0ad97329d9f892205
ThanosQween <52721631+ThanosQween@users.noreply.github.com>
2019-07-26 15:54:33 -0700

    Add files via upload

---
+++ breathe2.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+  window.setTimeout(fadeout, 7000); //8 seconds
+}
+window.setTimeout("document.getElementById('sgE-2035686-1-2-image').style.display='none';", 3000)
+
+function f1() {
+  document.getElementById('f1').style.opacity = '0';
+}
