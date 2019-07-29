8300468f4511d62d2200245ddf4dc651ac181883
ThanosQween <52721631+ThanosQween@users.noreply.github.com>
2019-07-28 21:43:30 -0700

    Update breathe2.js

--- breathe2.js
+++ breathe2.js
@@ -1,8 +1,13 @@
-window.onload = function() {
-  window.setTimeout(fadeout, 7000); //8 seconds
-}
-window.setTimeout("document.getElementById('sgE-2035686-1-2-image').style.display='none';", 3000)
-
-function f1() {
-  document.getElementById('f1').style.opacity = '0';
-}
+// window.onload = function() {
+//   window.setTimeout(fadeout, 7000); //8 seconds
+// }
+// window.setTimeout("document.getElementById('sgE-2035686-1-2-image').style.display='none';", 3000)
+//
+// function f1() {
+//   document.getElementById('f1').style.opacity = '0';
+// }
+<script type="text/javascript">
+setTimeout(function(){
+    document.getElementById('myimage').style.display = 'block';
+},600000);
+</script>
